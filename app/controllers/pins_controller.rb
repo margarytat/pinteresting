808669ab1e74dd65208eedf2ca3732e535cc236d
Margaryta Tchouikevitch <margaryta@gmail.com>
2017-11-20 11:32:14 -0500

    using earlier version of rack-test

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -50,6 +50,6 @@ class PinsController < ApplicationController
     end
 
     def pin_params
-      params.require(:pin).permit(:description, :image, :image_file_name)
+      params.require(:pin).permit(:description, :image)
     end
 end
